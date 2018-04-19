efffa707552695836ac294f763102f3251176575
Leonardo Merida Mejia <lemejia@microsoft.com>
2018-04-19 14:37:31 -0700

    Initial Commit
    Initial Commit. Correlation Vector implementation

---
+++ src/Microsoft.CorrelationVector.UnitTests/CorrelationVectorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.CorrelationVector.UnitTests
+{
+    [TestClass]
+    public class CorrelationVectorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Microsoft.CorrelationVector/CorrelationVectorVersion.cs
@@ -0,0 +1,10 @@
+﻿// <copyright>Copyright (c) Microsoft Corporation. All rights reserved.</copyright>
+
+namespace Microsoft.CorrelationVector
+{
+    public enum CorrelationVectorVersion
+    {
+        V1,
+        V2,
+    }
+}
